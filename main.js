cf138c07796cd917e2028d423c4e215b57b5b17f
Pedro Lucas <61566405+Pedro1516@users.noreply.github.com>
2022-03-30 13:39:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,23 +3,58 @@
    const btn = document.querySelector('#btn')
    const tela = document.querySelector('#tela')
    const texto = document.querySelector('#text')
-   const fotoInput = document.querySelctor('#foto-perfil')
+   const fotoInput = document.querySelector('#foto-perfil')
+   
+function registrar(nome, msg, img){
+  const obj = {
+    name: nome,
+    comentario: msg,
+    foto: img
+  }
+  const objJson = JSON.stringify(obj)
+  const position = localStorage.length 
+  localStorage.setItem(`comentario${position}`, objJson)
+}
 
-   btn.addEventListener('click', () => {
+function carregarComentarios(){
+  for(i = 0; i < localStorage.length; i++){
+    comentarioJson= localStorage.getItem(`comentario${i}`)
+  console.log(comentarioJson)
+  obj = JSON.parse(comentarioJson)
+  let perfil = ` <img src="${obj.foto}"><p id="nome">${obj.name} · Há poucos instantes `
+  let msg = `<p id="msg">\n ${obj.comentario}</p>`
+  let comentario = `<div class="name">${perfil}</div> ${msg}`
+  tela.innerHTML += `<div class="comentario"> ${comentario} </div>`
+}
+}
+
+btn.addEventListener('click', () => {
      if(nome.value.length != 0 && texto.value.length != 0){
        let comentComQuebra = texto.value
 
        for(i = 0; i < texto.value.length; i++){
          comentComQuebra =  comentComQuebra.replace('\n', '<br>')
        }
-
-      let perfil = ` <img src="img_perfil.png"><p id="nome">${nome.value} · Há poucos instantes `
+    
+      let files = fotoInput.files
+      
+      if(files.length == 0){
+        src = 'img_perfil.png'
+      }
+      else{
+         src = URL.createObjectURL(files[0])
+      }
+      
+      registrar(nome.value, comentComQuebra, src)
+      
+      let perfil = ` <img src="${src}"><p id="nome">${nome.value} · Há poucos instantes `
       let msg = `<p id="msg">\n ${comentComQuebra}</p>`
       let comentario = `<div class="name">${perfil}</div> ${msg}`
+      
       tela.innerHTML += `<div class="comentario"> ${comentario} </div>`
-   }
-   })
-
-fotoInput.addEventListener('change', () => {
-  console.log('Enviado')
+     }
 })
+
+document.onload = carregarComentarios()
+
+
